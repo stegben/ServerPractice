318e6cf6a6e3361362ddc1e9df165e9451aa3f41
cph <stegben.benjamin@gmail.com>
2016-04-05 19:15:39 +0800

    api query

--- app.js
+++ app.js
@@ -2,8 +2,16 @@ var express = require("express");
 var app = express();
 var router = express.Router();
 
-router.get("/", function(rep, res){
-  res.send("<h1>Home page</h1>")
+router.get("/", function(req, res){
+  res.send("<h1>Home page</h1>");
+});
+
+router.get("/api/query", function(req, res){
+  var res_json = {
+    x: req.query['x'],
+    y: req.query['y']
+  };
+  res.json(res_json);
 });
 
 app.use("/", router);
